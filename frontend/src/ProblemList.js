2ffa7ca0865cf31f1f4d98f78e19b8576d1e920e
신정환 <jhhhh@sinjeonghwan-ui-MacBookAir.local>
2022-10-30 14:47:59 +0900

    add tailwind

--- frontend/src/ProblemList.js
+++ frontend/src/ProblemList.js
@@ -1,2 +1,28 @@
 import React from "react";
-import axios from "axios";
+import axios, { Axios } from "axios";
+
+class ProblemList extends React.Component {
+
+    state = {
+
+    };
+
+    getProblemData = () => {
+        // 임시 데이터
+        const url = "https://jsonplaceholder.typicode.com/users";
+
+        Axios.get(url)
+            .then(
+                (res) => {
+
+                }
+            )
+            .catch(
+
+        );
+    };
+
+
+}
+
+export default ProblemList;
