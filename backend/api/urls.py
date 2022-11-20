94b57e572a55e3da6e36b976060cbeb7b8d15c2f
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-11-20 21:54:04 +0900

    api 작성

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,24 @@ from .views import *
 
 
 urlpatterns = [
-   path('problemlist/', ProblemListAPI),
-   path('problem/<int:problem_id>', ProblemAPI),
-   path('users/', UsersAPI),
+   path('problemlist', ProblemListAPI.as_view()),
+   path('problem/<int:problem_id>', ProblemAPI.as_view()),
+   path('userProblems/<int:user_id>', UserProblemAPI.as_view()),
+   path('users/<int:user_id>', UserListAPI.as_view()),
+   # path('users/<int:problem_id>', UserAPI),
+
+   path('answers', AnswerListAPI.as_view()),
+   path('answers/<int:answer_id>', AnswerAPI.as_view()),
+
+
+   path('testcase', TestCaseListAPI.as_view()),
+   path('testcase/<int:model_id>', TestCaseAPI.as_view()),
+
+   path('preset', PresetListAPI.as_view()),
+   path('preset/<int:user_id>/<int:problem_id>', PresetAPI.as_view()),
+
    path('login/', loginAPI),
    path('testcase/test/', codeTestAPI)
+   # path('testcase/test', ExecuteTestCaseAPI.as_view()),
+
 ]
