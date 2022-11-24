0b939aaccc8792111709178ce37f46fe026a4d19
unknown <kys00514@skku.edu>
2022-11-24 10:55:16 +0900

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,12 @@ import Left from "./Left";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Explanation from "./Explanation";
 import Related from "./Related";
+import Login from "./Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <div>
           <Routes>
             <Route
@@ -18,16 +18,16 @@ function App() {
               element={
                 <>
                   <ProblemList />
-                  <Explanation />
+                  <Explanation title="asdf" />
                 </>
               }
             />
+
             <Route path="/hhg" element={<Left />} />
+            <Route path="/login" element={<Login />} />
             {/* <Route path="/problems/" element={<ProblemDetail />} /> */}
           </Routes>
         </div>
-        {<Explanation title="asdf" />}
-        {<Related />}
       </div>
     </BrowserRouter>
   );
