749921d8732575b80cf46e4d7a00a1242e99c436
unknown <kys00514@skku.edu>
2022-11-27 23:10:50 +0900

    header

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,87 +3,82 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 import Button from "react-bootstrap/Button";
-import axios from "axios"
-import Popup from "./Popup"
+import axios from "axios";
+import Popup from "./Popup";
 
 class Login extends React.Component {
- 
   constructor(props) {
     super(props);
     this.state = {
-      "email": '',
-      "password": '',
-      seen: false
-      
-    }  
-   
-    
+      email: "",
+      password: "",
+      seen: false,
+    };
   }
   togglePop = () => {
- 
-   
     this.setState({
-     seen: !this.state.seen
+      seen: !this.state.seen,
     });
-  
- 
-    console.log(this.state.seen);
-   };
- handleChange = (e) => {
-    this.setState({"email": e.target.value});
-  
-}
- handleChange1 = (e) => {
-  this.setState({"password": e.target.value});
-  
- 
-}
-  handleClick() {
-    console.log("ID"+this.state.email);
-    console.log("PW"+this.state.password);
-    axios.defaults.withCredentials = true;
-   
-    axios.post('http://146.56.165.145:8000/api/login/',{"email":this.state.email,"password":this.state.password})
-    .then(response => {
-      console.log(response);
-      window.location.replace('/Problemlist');
-    })
 
+    console.log(this.state.seen);
+  };
+  handleChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+  handleChange1 = (e) => {
+    this.setState({ password: e.target.value });
+  };
+  handleClick() {
+    console.log("ID" + this.state.email);
+    console.log("PW" + this.state.password);
+    axios.defaults.withCredentials = true;
+
+    axios
+      .post("http://146.56.165.145:8000/api/login/", {
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then((response) => {
+        console.log(response);
+        window.location.replace("/Problemlist");
+      });
   }
 
   render() {
     return (
       <div className="Login">
-        <Form id='form'>
-        <Button id="new1" block size="lg" onClick={() => this.handleClick()}>
-          New Account
-        </Button>
+        <Form id="form">
+          <Button id="new1" block size="lg" onClick={() => this.handleClick()}>
+            New Account
+          </Button>
           <Form.Group size="lg" controlId="email" class="passr">
-
-
-
-            <Form.Control class="input" placeholder="ID" autoFocus type="text" onChange={(e) => this.handleChange(e)} />
+            <Form.Control
+              class="input"
+              placeholder="ID"
+              autoFocus
+              type="text"
+              onChange={(e) => this.handleChange(e)}
+            />
           </Form.Group>
 
           <Form.Group size="lg" controlId="password" class="passr">
-
-
-            <Form.Control class="input" placeholder="Password" type="password" onChange={(e) => this.handleChange1(e)} />
+            <Form.Control
+              class="input"
+              placeholder="Password"
+              type="password"
+              onChange={(e) => this.handleChange1(e)}
+            />
           </Form.Group>
           <Button id="new" block size="lg" onClick={this.togglePop}>
-          New Account
-        </Button>
+            New Account
+          </Button>
         </Form>
 
         <Button id="button" block size="lg" onClick={() => this.handleClick()}>
           Login
         </Button>
         {this.state.seen ? <Popup toggle={this.togglePop} /> : null}
-
-
       </div>
-   
-      
     );
   }
 }
--- frontend/src/ProblemList.js
+++ frontend/src/ProblemList.js
@@ -1,81 +1,67 @@
 import React from "react";
 import axios from "axios";
 import Tr from "./Tr";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-
+import Header from "./Header";
 class ProblemList extends React.Component {
-    state = {
-        ProblemData: [],
-        "flag": false
-    };
-    shouldComponentUpdate() {
-        if ( this.state.flag ) {
-          return false;
-        }
-        return true;
-      }
+  state = {
+    ProblemData: [],
+    flag: false,
+  };
+  shouldComponentUpdate() {
+    if (this.state.flag) {
+      return false;
+    }
+    return true;
+  }
 
-    getProblemData = () => {
-       console.log(1);
-        // 임시 데이터 호출
-        const base = "http://146.56.165.145:8000/api/problemlist";
+  getProblemData = () => {
+    console.log(1);
+    // 임시 데이터 호출
+    const base = "http://146.56.165.145:8000/api/problemlist";
 
-        axios.get(base)
-            .then(
-                (res) => {
-                    console.log(res.data);
+    axios
+      .get(base)
+      .then((res) => {
+        console.log(res.data);
 
-                    // 반환 형식 아직 정의되지 않음, 임시
-                    this.setState({
-                        ProblemData: res.data,
-                        flag: true
+        // 반환 형식 아직 정의되지 않음, 임시
+        this.setState({
+          ProblemData: res.data,
+          flag: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log("done");
+  };
 
-                    });
-                }
-            )
-            .catch(
-                (err) => {
-                    console.log(err);
-                }
-            );
-            console.log("done");
-    };
+  render() {
+    {
+      this.getProblemData();
+    }
 
-    render() {
-        
-        {this.getProblemData()}
+    return (
+      <div className="container max-w-screen-lg mx-auto">
+        <Header />
+        <div className="text-xl font-bold mt-5 mb-3 text-center">문제 목록</div>
 
-        return (
-        
-           
-            <div  className="container max-w-screen-lg mx-auto">
-              
-                <div className="text-xl font-bold mt-5 mb-3 text-center" >
-                    문제 목록
-                </div>
-
-                <table className="min-w-full table-auto text-gray-800">
-                    <thead className="justify-between">
-                        <tr className="bg-gray-800">
-                            <th className="text-gray-300 px-4 py-3">번호</th>
-                            <th className="text-gray-300 px-4 py-3">문제명</th>
-                            <th className="text-gray-300 px-4 py-3">난이도</th>
-                            <th className="text-gray-300 px-4 py-3">정답률</th>
-                            <th className="text-gray-300 px-4 py-3">내 점수</th>
-                        </tr>
-                    </thead>
-                    <Tr ProblemData={this.state.ProblemData} />
-
-                </table>
-               
-              
-            </div>
-           
-        );
-
-    };
-    return;
-};
+        <table className="min-w-full table-auto text-gray-800">
+          <thead className="justify-between">
+            <tr className="bg-gray-800">
+              <th className="text-gray-300 px-4 py-3">번호</th>
+              <th className="text-gray-300 px-4 py-3">문제명</th>
+              <th className="text-gray-300 px-4 py-3">난이도</th>
+              <th className="text-gray-300 px-4 py-3">정답률</th>
+              <th className="text-gray-300 px-4 py-3">내 점수</th>
+            </tr>
+          </thead>
+          <Tr ProblemData={this.state.ProblemData} />
+        </table>
+      </div>
+    );
+  }
+  return;
+}
 
 export default ProblemList;
