16386aa345a4ba3655643d57fe5e111210a6b158
unknown <kys00514@skku.edu>
2022-11-29 16:38:17 +0900

    result

---
+++ frontend/src/Result.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [44, 55, 41, 17, 15],
+      options: {
+        chart: {
+          type: "donut",
+        },
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200,
+              },
+              legend: {
+                position: "bottom",
+              },
+            },
+          },
+        ],
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div id="chart">
+        <Chart
+          options={this.state.options}
+          series={this.state.series}
+          type="donut"
+          height={200}
+          width={400}
+        />
+      </div>
+    );
+  }
+}
+
+export default Result;
