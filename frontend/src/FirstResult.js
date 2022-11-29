4dadd6b3457e31b8c90230feeffa03379627b52f
unknown <kys00514@skku.edu>
2022-11-29 17:29:49 +0900

    tab

---
+++ frontend/src/FirstResult.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+class FirstResult extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="FirstTab">
+          <p>First Tab!! Hurray!!</p>
+          {/* First tab content will go here */}
+        </div>
+      </>
+    );
+  }
+}
+
+export default FirstResult;
--- frontend/src/Result.js
+++ frontend/src/Result.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
+import { useState } from "react";
+import SecondResult from "./SecondResult";
+import FirstResult from "./FirstResult";
 class Result extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      series: [44, 55, 41, 17, 15],
+      series: [25, 15, 44, 55, 41, 17],
       options: {
         chart: {
           type: "donut",
         },
+        labels: [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ],
         responsive: [
           {
             breakpoint: 480,
@@ -24,20 +35,63 @@ class Result extends React.Component {
           },
         ],
       },
+      activeTab: -1,
     };
   }
+  handleTab1 = () => {
+    this.setState({
+      activeTab: 1,
+    });
+    console.log(1);
+  };
+
+  handleTab2 = () => {
+    this.setState({
+      activeTab: 2,
+    });
+  };
 
   render() {
     return (
-      <div id="chart">
-        <Chart
-          options={this.state.options}
-          series={this.state.series}
-          type="donut"
-          height={200}
-          width={400}
-        />
-      </div>
+      <>
+        <div id="chart">
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="donut"
+            height={200}
+            width={400}
+          />
+        </div>
+        <div className="Tabs">
+          {/* Tab nav */}
+          <ul className="nav">
+            <li
+              className={this.state.activeTab === 1 ? "active" : ""}
+              onClick={() => this.handleTab1()}
+            >
+              Tab 1
+            </li>
+            <li
+              className={this.state.activeTab === 2 ? "active" : ""}
+              onClick={() => this.handleTab2()}
+            >
+              Tab 2
+            </li>
+          </ul>
+          <div className="outlet">
+            {
+              <>
+                {this.state.activeTab === 1 ? (
+                  <FirstResult />
+                ) : (
+                  <SecondResult />
+                )}
+              </>
+            }
+          </div>
+        </div>
+      </>
     );
   }
 }
