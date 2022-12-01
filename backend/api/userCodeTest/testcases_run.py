adcc029236f2c54a0bd81e4327cfe72190ddeca3
JackCokebb <reo2726@gmail.com>
2022-12-01 21:23:01 +0900

    docker testcase enhencement

--- backend/api/userCodeTest/testcases_run.py
+++ backend/api/userCodeTest/testcases_run.py
@@ -1,15 +1,22 @@
 import unittest
 import test
+import traceback
 class solutionTest(unittest.TestCase):
 	def test0(self):
 		try:
 			self.assertEqual(test.solution(2, 3),-1)
-		except AssertionError as e:
-			print(f'fail:0') 
+			print(f'success:0')
+			print(str(test.solution(2, 3)))
+		except Exception as error:
+			print('fail:0')
+			print(error)
 	def test1(self):
 		try:
 			self.assertEqual(test.solution(102, 12),10)
-		except AssertionError as e:
-			print(f'fail:1') 
+			print(f'success:1')
+			print(str(test.solution(102, 12)))
+		except Exception as error:
+			print('fail:1')
+			print(error)
 if __name__ == '__main__':
 	unittest.main()
