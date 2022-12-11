f11f99499777cc50a1da8510f9cd84762a5d45ef
unknown <kys00514@skku.edu>
2022-12-11 12:49:45 +0900

    main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
               path="/"
               element={
                 <>
-                  <ProblemList />
+                  <Login />
                 </>
               }
             />
